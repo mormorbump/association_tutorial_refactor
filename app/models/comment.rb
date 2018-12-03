e3e1673d82d7b71ce2f4cff2dd006872a85e250c
kazuki matsumoto <mormorbump@gmail.com>
2018-12-03 16:32:14 +0900

    validate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :tweet
+    validates :body, presence: true, length: { maximum: 140 }
+    validates :tweet_id, presence: true
+    validates :user_id, presence: true
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,8 @@ class Tweet < ApplicationRecord
     belongs_to :user
     has_many :favorites
     has_many :comments
+    validates :body, presence: true, length: { maximum: 140 }
+    validates :user_id, presence: true
 
     def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
